22a4433d33882bcc2dc6bcbb76f39f9fb9cf5f26
HSavynetska <hsavynetska@luxoft.com>
2018-04-24 15:48:08 +0300

    Test_scripts for rpc:AddSubMenu, menuIcon

---
+++ test_scripts/API/SubMenuIcon/004_menuIcon_is_absent.lua
@@ -0,0 +1,54 @@
+---------------------------------------------------------------------------------------------------
+-- Proposal: https://github.com/smartdevicelink/sdl_evolution/blob/master/proposals/0085-submenu-icon.md
+-- User story:TBD
+-- Use case:TBD
+--
+-- Requirement summary:
+-- TBD
+--
+-- Description:
+-- In case:
+-- 1) 
+-- SDL does:
+-- 1) 
+---------------------------------------------------------------------------------------------------
+--[[ Required Shared libraries ]]
+local runner = require('user_modules/script_runner')
+local common = require('test_scripts/API/SubMenuIcon/commonSubMenuIcon')
+
+--[[ Test Configuration ]]
+runner.testSettings.isSelfIncluded = false
+
+--[[ Local Variables ]]
+local requestParams = {
+	menuID = 1000,
+	position = 500,
+	menuName ="SubMenupositive",
+}
+
+local responseUiParams = {
+	menuID = requestParams.menuID,
+	menuParams = {
+		position = requestParams.position,
+		menuName = requestParams.menuName,
+	}
+}
+
+local allParams = {
+	requestParams = requestParams,
+	responseUiParams = responseUiParams
+}
+
+--[[ Scenario ]]
+runner.Title("Preconditions")
+runner.Step("Clean environment", common.preconditions)
+runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+runner.Step("App registration", common.registerAppWOPTU, { 1 })
+runner.Step("Activate Application", common.activateApp, { 1 })
+runner.Step("Upload icon file", common.putFile)
+
+runner.Title("Test")
+runner.Step("AddSubMenu ", common.AddSubMenu, {allParams, 1 })
+
+runner.Title("Postconditions")
+runner.Step("Stop SDL", common.postconditions)
